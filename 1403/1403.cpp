a175da08f39dc2dd9421428304fe9f712c450df4
MatthewCoff <miramavs@gmail.com>
2023-03-14 22:52:44 +0200

    Добавьте файлы проекта.

---
+++ 1403/1403.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+//пример черепахи и панцыря. Композиция. Черепаха - целая часть. Панцырь - неотльемлемая часть. Без панцыря черепахи не будет:(
+class Turtle {
+
+	public:
+
+	Turtle(string name, string variety, int age)
+		: turName(name), turVariety(variety), turAge(age), turShell(Shell()) {}
+	void procherepashku() {
+		cout << "Name: " << turName << endl;
+		cout << "variety: " << turVariety << endl;
+		cout << "Age: " << turAge << endl;
+
+		cout << "Shell color: " << turShell.datColor() << endl;
+	
+	}
+	private:
+	class Shell {
+	public:
+		Shell() : m_color("Green"){}
+		string datColor() {
+			return m_color;
+		}
+		
+	private:
+		string m_color;
+		int m_size;
+	};
+	string turName;
+	string turVariety;
+	int turAge;
+
+	string turColor;	
+
+	Shell turShell;
+};
+
+int main() {
+	Turtle gavriil("Gavriil", "BigGreen", 45);
+	gavriil.procherepashku();
+
+
+	Turtle hto("HtoCe", "SmallBlue", 3);
+	hto.procherepashku();
+	return 0;
+}
